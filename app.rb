84b0d0fa93315dec37cf5a78571ca17931cb4faf
Saint-Jean Germila <germila@noukod.com>
2019-01-14 19:00:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,24 +2,17 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	# get '/' do
-	# 	"<h1>Hello World</h1>"
-	# end
-
 get '/' do
 	erb :index
 end
 
-# get '/' do
-# 	erb :index
-# end
-
-# get "/info" do
-# 	"Testing the info page"
-# end
+get '/'do
+	erb :info
+end
 
 get "/info" do
 	erb :dogs
 end
+
 
 end
